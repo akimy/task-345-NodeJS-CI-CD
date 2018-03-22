f38f4740728dec281c297cb0f06d6412861a23b2
Alexander Vaganov <ar.vaganov@gmail.com>
2018-03-23 02:05:31 +0300

    Revert "shell script for get local branches"
    This reverts commit 6bf7b6262f3523727b4a33f5435fb2aa8d12c1d2.

--- getbranches.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for branch in $(git --git-dir ./repository/.git branch --all | grep '^\s*remotes' | egrep --invert-match '(:?HEAD|master)$'); do
-    git --git-dir ./repository/.git branch --track "${branch##*/}" "$branch"
-done
